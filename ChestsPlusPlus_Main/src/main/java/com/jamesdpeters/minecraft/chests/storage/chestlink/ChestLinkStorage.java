c71c8d62147840157d3dc861d70b32f3f59dc040
jameslfc19 <jameslfc19@gmail.com>
2020-07-14 19:26:32 +0100

    Update ChestLinkStorage.java
    When adding a chest that already contains an item, the display item now gets updated!

--- ChestsPlusPlus_Main/src/main/java/com/jamesdpeters/minecraft/chests/storage/chestlink/ChestLinkStorage.java
+++ ChestsPlusPlus_Main/src/main/java/com/jamesdpeters/minecraft/chests/storage/chestlink/ChestLinkStorage.java
@@ -51,6 +51,7 @@ public class ChestLinkStorage extends AbstractStorage implements ConfigurationSe
             Chest chest = (Chest) block.getState();
             getInventory().setContents(chest.getInventory().getContents());
             chest.getInventory().clear();
+            updateDisplayItem();
         }
         init();
     }
