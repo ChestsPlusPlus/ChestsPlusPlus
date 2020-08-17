692af7902ef2187d0682869aa5f29ef1b0d6c32f
jameslfc19 <jameslfc19@gmail.com>
2020-08-17 16:48:29 +0100

    Update NMSProviderImpl.java
    Fix for 1.16.2 crash

--- ChestsPlusPlus_1_16/src/main/java/com/jamesdpeters/minecraft/chests/v1_16_R1/NMSProviderImpl.java
+++ ChestsPlusPlus_1_16/src/main/java/com/jamesdpeters/minecraft/chests/v1_16_R1/NMSProviderImpl.java
@@ -3,13 +3,28 @@ package com.jamesdpeters.minecraft.chests.v1_16_R1;
 import com.jamesdpeters.minecraft.chests.ChestOpener;
 import com.jamesdpeters.minecraft.chests.MaterialChecker;
 import com.jamesdpeters.minecraft.chests.NMSProvider;
+import com.jamesdpeters.minecraft.chests.TileEntityOpener;
+import org.bukkit.block.Container;
+import org.bukkit.block.Lidded;
 import org.bukkit.entity.ItemFrame;
+import org.bukkit.inventory.Inventory;
 
 public class NMSProviderImpl implements NMSProvider {
 
     @Override
     public ChestOpener getChestOpener() {
-        return new ChestOpener_1_16();
+        return (inventory, container, tileEntityOpener) -> {
+            if(hasLiddedAPI()){
+                if(container instanceof Lidded){
+                    if(inventory.getViewers().size() > 0){
+                        ((Lidded) container).open();
+                    } else {
+                        ((Lidded) container).close();
+                    }
+                }
+            }
+            return null;
+        };
     }
 
     @Override
@@ -20,5 +35,14 @@ public class NMSProviderImpl implements NMSProvider {
     @Override
     public void setItemFrameVisible(ItemFrame itemFrame, boolean visible) {
         itemFrame.setVisible(visible);
+    }
+
+    private boolean hasLiddedAPI(){
+        try {
+            Class.forName("org.bukkit.block.Lidded");
+            return true;
+        } catch (ClassNotFoundException e){
+            return false;
+        }
     }
 }
