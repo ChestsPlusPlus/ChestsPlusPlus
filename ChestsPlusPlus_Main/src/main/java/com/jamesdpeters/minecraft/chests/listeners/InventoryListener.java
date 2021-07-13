d7e07c042b3377c624a8195c932ac8247f7cc3e2
James Peters <jameslfc19@gmail.com>
2021-07-14 00:14:36 +0100

    Fix Build

--- ChestsPlusPlus_Main/src/main/java/com/jamesdpeters/minecraft/chests/listeners/InventoryListener.java
+++ ChestsPlusPlus_Main/src/main/java/com/jamesdpeters/minecraft/chests/listeners/InventoryListener.java
@@ -10,7 +10,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.*;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
@@ -26,8 +30,7 @@ public class InventoryListener implements Listener {
     public void onInventoryClose(InventoryCloseEvent event) {
         try {
             InventoryHolder holder = event.getInventory().getHolder();
-            if (holder instanceof VirtualInventoryHolder) {
-                VirtualInventoryHolder vHolder = (VirtualInventoryHolder) holder;
+            if (holder instanceof VirtualInventoryHolder vHolder) {
                 vHolder.openPreviousInventory();
                 if (vHolder.didPlayerRemoteOpen(event.getPlayer().getUniqueId())) {
                     Utils.closeInventorySound((Player) event.getPlayer(), event.getInventory());
