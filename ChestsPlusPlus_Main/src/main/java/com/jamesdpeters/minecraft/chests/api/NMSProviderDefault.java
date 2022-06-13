907d1b76733bb201edfd125f29494225f68f2e9c
ceze88 <bdenes.home@gmail.com>
2022-06-13 17:03:26 +0200

    1.19 support

--- ChestsPlusPlus_Main/src/main/java/com/jamesdpeters/minecraft/chests/api/NMSProviderDefault.java
+++ ChestsPlusPlus_Main/src/main/java/com/jamesdpeters/minecraft/chests/api/NMSProviderDefault.java
@@ -1,6 +1,11 @@
 package com.jamesdpeters.minecraft.chests.api;
 
-import com.jamesdpeters.minecraft.chests.*;
+import com.jamesdpeters.minecraft.chests.ChestOpener;
+import com.jamesdpeters.minecraft.chests.ChestsPlusPlus;
+import com.jamesdpeters.minecraft.chests.CraftingProvider;
+import com.jamesdpeters.minecraft.chests.EntityEventListener;
+import com.jamesdpeters.minecraft.chests.MaterialChecker;
+import com.jamesdpeters.minecraft.chests.NMSProvider;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.ItemFrame;
 
@@ -16,9 +21,10 @@ public class NMSProviderDefault implements NMSProvider {
             case "v1_17_R1" -> defaultProvider = new com.jamesdpeters.minecraft.chests.v1_17_R1.NMSProviderImpl();
             case "v1_18_R1" -> defaultProvider = new com.jamesdpeters.minecraft.chests.v1_18_R1.NMSProviderImpl();
             case "v1_18_R2" -> defaultProvider = new com.jamesdpeters.minecraft.chests.v1_18_R2.NMSProviderImpl();
+            case "v1_19_R1" -> defaultProvider = new com.jamesdpeters.minecraft.chests.v1_19_R1.NMSProviderImpl();
             default -> {
                 ChestsPlusPlus.PLUGIN.getLogger().severe("§c=======================================================");
-                ChestsPlusPlus.PLUGIN.getLogger().severe("§cThis version is not supported. Please update your server!");
+                ChestsPlusPlus.PLUGIN.getLogger().severe("§cThis version is not supported. Please update your server or the plugin!");
                 ChestsPlusPlus.PLUGIN.getLogger().severe("§c=======================================================");
                 defaultProvider = new com.jamesdpeters.minecraft.chests.v1_17_R1.NMSProviderImpl();
             }
